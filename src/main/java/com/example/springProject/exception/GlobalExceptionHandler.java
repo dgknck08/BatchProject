be7d5d4def642f74ae7312fbed166ea2ab238185
dgknck08 <dogu08cicek@gmail.com>
2025-06-18 13:23:45 +0300

    GlobalExceptionHandling class added.

--- src/main/java/com/example/springProject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/springProject/exception/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package com.example.springProject.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;	
 
 import java.util.Map;
 
